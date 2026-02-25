82feee073ee453edfc915bf1683dd8ce9716d376
amanRh (rchmnnr) <139310389+rchmnnr@users.noreply.github.com>
2026-02-26 06:31:07 +0700

    Refactor text strings and add contest results messages

--- data/text/std_text.asm
+++ data/text/std_text.asm
@@ -19,37 +19,30 @@ NurseEveText:
 NurseNiteText:
 	text "Selamat malam!"
 	line "Anda belum tidur."
-
 	para "Selamat datang di"
 	line "Klinik #mon."
 	done
 
 PokeComNurseMornText:
 	text "Selamat pagi!"
-
 	para "Ini Pusat Komu-"
 	line "nikasi #mon-"
-
 	para "atau disebut juga"
 	line "Pusat #Com."
 	done
 
 PokeComNurseDayText:
 	text "Halo!"
-
 	para "Ini Pusat Komu-"
 	line "nikasi #mon-"
-
 	para "atau disebut juga"
 	line "Pusat #Com."
 	done
 
 PokeComNurseEveText:
 	text "Selamat malam!"
-
 	para "Ini Pusat Komu-"
 	line "nikasi #mon-"
-
 	para "atau disebut juga"
 	line "Pusat #Com."
 	done
@@ -57,10 +50,8 @@ PokeComNurseEveText:
 PokeComNurseNiteText:
 	text "Anda hebat,"
 	line "semangat lembur."
-
 	para "Ini Pusat Komu-"
 	line "nikasi #mon-"
-
 	para "atau disebut juga"
 	line "Pusat #Com."
 	done
@@ -69,7 +60,6 @@ NurseAskHealText:
 	text "Kami bisa obati"
 	line "#mon Anda"
 	cont "sampai pulih."
-
 	para "Mulai pulihkan"
 	line "#mon Anda?"
 	done
@@ -78,18 +68,13 @@ NurseTrainerStarText:
 	text "Kami bisa obati"
 	line "#mon Anda"
 	cont "sampai pulih."
-
 	para "Boleh kami…"
-
 	para "Wah, itu Kartu"
 	line "Tanda Pelatih!"
-
 	para "Warna yang langka!"
 	line "Semua bintang itu!"
-
 	para "Kamu Pelatih yang"
 	line "sangat hebat."
-
 	para "Boleh kami obati"
 	line "#mon Anda,"
 	cont "<PLAYER>?"
@@ -98,7 +83,6 @@ NurseTrainerStarText:
 NurseTheUsualText:
 	text "Senang berjumpa"
 	line "lagi, <PLAYER>!"
-
 	para "Saya tahu, pasti"
 	line "seperti biasa?"
 	done
@@ -112,7 +96,6 @@ NurseTakePokemonText:
 NurseReturnPokemonText:
 	text "Terima kasih"
 	line "sudah menunggu."
-
 	para "#mon Anda sudah"
 	line "sehat kembali."
 	done
@@ -125,17 +108,13 @@ NurseGoodbyeText:
 NursePokerusText:
 	text "#mon Anda"
 	line "tampaknya sedang"
-
 	para "terinfeksi oleh"
 	line "virus langka."
-
 	para "#mon Anda sehat"
 	line "dan sistem imun"
 	cont "mereka normal."
-
 	para "Tapi kami tidak"
 	line "tahu lebih banyak"
-
 	para "di sini, di"
 	line "Klinik #mon."
 	done
@@ -143,10 +122,8 @@ NursePokerusText:
 PokeComNursePokerusText:
 	text "#mon Anda"
 	line "tampaknya sedang"
-
 	para "terinfeksi oleh"
 	line "virus langka."
-
 	para "#mon Anda sehat"
 	line "dan sistem imun"
 	cont "mereka normal."
@@ -165,20 +142,16 @@ PictureBookshelfText:
 MagazineBookshelfText:
 	text "Majalah #mon…"
 	line "Sobat #mon,"
-
 	para "Buku Saku #mon,"
 	line "Grafik #mon…"
 	done
 
 TeamRocketOathText:
 	text "Sumpah Tim Rocket"
-
 	para "Curi #mon demi"
 	line "keuntungan!"
-
 	para "Peras #mon demi"
 	line "keuntungan!"
-
 	para "Semua #mon ada"
 	line "demi kejayaan"
 	cont "Tim Rocket!"
@@ -245,6 +218,208 @@ PokeCenterSignText:
 MartSignText:
 	text "Toko Kebutuhan"
 	line "#mon Anda"
-
 	para "Mart #mon"
+	done
+
+ContestResults_ReadyToJudgeText:
+	text "Kami akan menilai"
+	line "#mon yang Anda"
+	cont "tangkap."
+	para "……"
+	line "……"
+	para "Kami telah memilih"
+	line "pemenangnya!"
+	para "Anda siap untuk"
+	line "hasilnya?"
+	done
+
+ContestResults_PlayerWonAPrizeText:
+	text "<PLAYER>, juara ke-"
+	text_ram wStringBuffer3
+	line ", mendapatkan"
+	cont ""
+	text_ram wStringBuffer4
+	text "!"
+	done
+
+ContestResults_JoinUsNextTimeText:
+	text "Silakan bergabung"
+	line "di Kontes nanti!"
+	done
+
+ContestResults_ConsolationPrizeText:
+	text "Peserta lain dapat"
+	line ""
+	text_ram wStringBuffer4
+	text " sebagai"
+	cont "hadiah hiburan!"
+	done
+
+ContestResults_DidNotWinText:
+	text "Semoga beruntung"
+	line "di lain waktu."
+	done
+
+ContestResults_ReturnPartyText:
+	text "Kami kembalikan"
+	line "#mon yang kami"
+	para "titipkan."
+	line "Ini dia!"
+	done
+
+ContestResults_PartyFullText:
+	text "Party Anda penuh,"
+	line "jadi #mon Anda"
+	cont "dikirim ke PC."
+	done
+
+GymStatue_CityGymText:
+	text_ram wStringBuffer3
+	line "Gym #mon"
+	para "Pemimpin: "
+	text_ram wStringBuffer4
+	done
+
+GymStatue_WinningTrainersText:
+	text "Pelatih Pemenang:"
+	line "<RIVAL>"
+	done
+
+GymStatue_TwoWinningTrainersText:
+	text "Pelatih Pemenang:"
+	line "<RIVAL>"
+	cont "<PLAYER>"
+	done
+
+GymStatue_ThreeWinningTrainersText:
+	text "Pelatih Pemenang:"
+	line "<RIVAL>"
+	cont "<PLAYER>"
+	cont "Lyra"
+	done
+
+CoinVendor_WelcomeText:
+	text "Selamat datang di"
+	line "Game Corner."
+	done
+
+CoinVendor_NoCoinCaseText:
+	text "Butuh koin game?"
+	para "Oh, Anda tidak"
+	line "punya Coin Case"
+	cont "untuk koin Anda."
+	done
+
+CoinVendor_IntroText:
+	text "Butuh beberapa"
+	line "koin game?"
+	para "Harganya ¥1000"
+	line "untuk 50 koin."
+	cont "Mau beli?"
+	done
+
+CoinVendor_Buy50CoinsText:
+	text "Terima kasih!"
+	line "Ini 50 koinnya."
+	done
+
+CoinVendor_Buy500CoinsText:
+	text "Terima kasih!"
+	line "Ini 500 koinnya."
+	done
+
+CoinVendor_NotEnoughMoneyText:
+	text "Uang Anda tidak"
+	line "cukup."
+	done
+
+CoinVendor_CoinCaseFullText:
+	text "Ups! Coin Case"
+	line "Anda sudah penuh."
+	done
+
+CoinVendor_CancelText:
+	text "Tidak jadi beli?"
+	line "Datang lagi, ya!"
+	done
+
+BugContestPrizeNoRoomText:
+	text "Oh? Tas Anda"
+	line "sudah penuh."
+	para "Kami simpan dulu"
+	line "hari ini, jadi"
+	para "kembalilah saat"
+	line "ada ruang kosong."
+	done
+
+HappinessText3:
+	text "Wow! Kamu dan"
+	line "#mon kamu sangat"
+	cont "akrab!"
+	done
+
+HappinessText2:
+	text "#mon jadi lebih"
+	line "ramah jika kamu"
+	para "menghabiskan waktu"
+	line "bersama mereka."
+	done
+
+HappinessText1:
+	text "Kamu belum jinak"
+	line "dengan #monmu."
+	para "Jika tidak baik,"
+	line "ia akan cemberut."
+	done
+
+RegisteredNumber1Text:
+	text "<PLAYER> simpan"
+	line "nomor HP"
+	cont ""
+	text_ram wStringBuffer3
+	text "."
+	done
+
+RegisteredNumber2Text:
+	text "<PLAYER> simpan"
+	line "nomor HP"
+	cont ""
+	text_ram wStringBuffer3
+	text "."
+	done
+
+VendingMachineText:
+	text "Mesin penjual!"
+	line "Ini menunya."
+	done
+
+VendingMachineClangText:
+	text "Klang!"
+	para ""
+	text_ram wStringBuffer3
+	line "keluar."
+	done
+
+VendingMachineScoreText:
+	text "Hoki! Ekstra satu"
+	line ""
+	text_ram wStringBuffer3
+	cont "keluar."
+	done
+
+VendingMachineNoMoneyText:
+	text "Ups, uang tidak"
+	line "cukup…"
+	done
+
+VendingMachineNoSpaceText:
+	text "Sudah tidak ada"
+	line "ruang lagi…"
+	done
+
+HiddenGrottoText:
+	text "Lihat! Kamu nemu"
+	line "jalan sempit!"
+	para "Mau masuk ke"
+	line "dalam?"
 	done
